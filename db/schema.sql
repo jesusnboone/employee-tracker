b70877c03f5a65b0f63a5bbb3193ff5a61cc83d4
jesusnboone <jesusnboone@gmail.com>
2020-08-06 13:53:51 -0500

    fixed schema and seeds syntax

--- db/schema.sql
+++ db/schema.sql
@@ -2,29 +2,32 @@ DROP DATABASE IF EXISTS employees_db;
 CREATE DATABASE employees_db;
 USE employees_db;
 
-DROP TABLE IF EXISTS Department;
-DROP TABLE IF EXISTS Role;
-DROP TABLE IF EXISTS Employee;
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS employee;
 
-CREATE TABLE Department(
-    id INT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL
+CREATE TABLE department(
+    id INTEGER(11) AUTO_INCREMENT NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE Role(
-    id INT PRIMARY KEY,
+CREATE TABLE role(
+    id INTEGER(11) AUTO_INCREMENT NOT NULL,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL,
-    department_id INT
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES Department(id) ON DELETE SET NULL
+    department_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
-CREATE TABLE Employee(
-    id INT PRIMARY KEY,
+CREATE TABLE employee(
+    id INTEGER(11) AUTO_INCREMENT NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
-    manager_id INT
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES Role(id) ON DELETE SET NULL
-    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES Role(title) ON DELETE SET NULL
+    manager_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES role(id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -7,29 +7,29 @@ VALUES
 
 INSERT INTO Role (title, salary, department_id)
 VALUES
-('Cultivator', '$35k', 1),
-('Lead Cultivator', '$45k', 1),
-('Cultivation Manager', '$60k', 1),
-('Distributor', '$40k', 2),
-('Distribution Manager', '$50k', 2),
-('Packager', '$30k', 3),
-('Packaging Lead', '$35k', 3),
-('Packaging Manager', '$45k', 3),
-('Trimmer', '$30k', 4),
-('Trim Lead', '$35k', 4),
-('Trim Manager', '$45k', 4);
+('Cultivator', '35000', 1),
+('Lead Cultivator', '45000', 1),
+('Cultivation Manager', '60000', 1),
+('Distributor', '40000', 2),
+('Distribution Manager', '50000', 2),
+('Packager', '30000', 3),
+('Packaging Lead', '35000', 3),
+('Packaging Manager', '45000', 3),
+('Trimmer', '30000', 4),
+('Trim Lead', '35000', 4),
+('Trim Manager', '45000', 4);
 
 INSERT INTO Employee (first_name, last_name, role_id, manager_id)
 VALUES
 ('John', 'McMahon', 1, 3),
 ('William', 'Adams', 2, 3),
-('Kelley', 'Roberts', 3),
+('Kelley', 'Roberts', 3, NULL),
 ('Michael', 'Jones', 4, 5),
-('Ben', 'Scott', 5),
+('Ben', 'Scott', 5, NULL),
 ('Samantha', 'Jameson', 6, 8),
 ('Angel', 'Ramirez', 7, 8),
-('Josh', 'Griffiths', 8),
+('Josh', 'Griffiths', 8, NULL),
 ('Jacob', 'Peterson', 9, 11),
 ('Caitlin', 'Richards', 10, 11),
-('Devon', 'Devine', 11);
+('Devon', 'Devine', 11, NULL);
 
