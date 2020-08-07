0b02e91f5fac69becceb63d3df443a2c9fb6ecfe
jesusnboone <jesusnboone@gmail.com>
2020-08-07 16:54:42 -0500

    added inquirer menu

--- db/database.js
+++ db/database.js
@@ -1,4 +1,6 @@
 const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const cTable = require('console.table');
 
 // Creates the connection to database
 const connection = mysql.createConnection({
@@ -13,16 +15,14 @@ const connection = mysql.createConnection({
 
 connection.connect(err => {
   if (err) throw err;
-  console.log('connected as id ' + connection.threadId);
 ////////////////////////////////////
-employeeAll();
 ////^^^INSERT FUNCTION HERE TO TEST^^^///
 });
 
 departmentAll = () => {
   connection.query('SELECT * FROM department', function(err, res) {
     if (err) throw err;
-    console.log(res);
+    console.table(res);
     connection.end();
   });
 };
@@ -30,7 +30,7 @@ departmentAll = () => {
 roleAll = () => {
   connection.query('SELECT * FROM role', function(err, res) {
     if (err) throw err;
-    console.log(res);
+    console.table(res);
     connection.end();
   });
 };
@@ -38,7 +38,45 @@ roleAll = () => {
 employeeAll = () => {
   connection.query('SELECT a.id, a.first_name, a.last_name, CONCAT(b.first_name, " ", b.last_name) AS manager FROM employee a LEFT JOIN employee b ON a.manager_id = b.id', function(err, res) {
     if (err) throw err;
-    console.log(res);
+    console.table(res);
     connection.end();
   });
 };
+
+function menu() {
+
+  function createManager() {
+    console.log("EMPLOYEE TRACKER");
+    inquirer.prompt([
+      {
+        type: "list",
+        name: "optionSelect",
+        message: "What Would You Like To Do?",
+        choices: [
+          "view all DEPARTMENTS",
+          "view all ROLES",
+          "view all EMPLOYEES",
+          "add a DEPARTMENT",
+          "add a ROLE",
+          "add an EMPLOYEE",
+          "update an EMPLOYEE ROLE"
+        ]
+      }
+    ])
+  .then(userChoice => {
+    switch (userChoice.optionSelect) {
+      case "view all DEPARTMENTS":
+        departmentAll();
+        break;
+      case "view all ROLES":
+        roleAll();
+        break;
+      case "view all EMPLOYEES":
+        employeeAll();
+        break;
+    }
+  })};
+  createManager();
+}
+
+menu();
