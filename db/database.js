ea40c257e20515e310e40769357721668df3380c
jesusnboone <jesusnboone@gmail.com>
2020-08-07 14:29:24 -0500

    updated mysql functions for queries

--- db/database.js
+++ db/database.js
@@ -1,27 +1,44 @@
-// get the client
 const mysql = require('mysql2');
- 
-// create the connection to database
+
+// Creates the connection to database
 const connection = mysql.createConnection({
   host: 'localhost',
+  port: 3306,
+  // Your MySQL username
   user: 'root',
+  // Your MySQL password
+  password: '',
   database: 'employees_db'
 });
- 
-// simple query
-connection.query(
-  'SELECT * FROM `employee`',
-  function(err, results, fields) {
-    console.log(results); // results contains rows returned by server
-    console.log(fields); // fields contains extra meta data about results, if available
-  }
-);
- 
-/* with placeholder
-connection.query(
-  'SELECT * FROM `table` WHERE `name` = ? AND `age` > ?',
-  ['Page', 45],
-  function(err, results) {
-    console.log(results);
-  }
-);*/
+
+connection.connect(err => {
+  if (err) throw err;
+  console.log('connected as id ' + connection.threadId);
+////////////////////////////////////
+employeeAll();
+////^^^INSERT FUNCTION HERE TO TEST^^^///
+});
+
+departmentAll = () => {
+  connection.query('SELECT * FROM department', function(err, res) {
+    if (err) throw err;
+    console.log(res);
+    connection.end();
+  });
+};
+
+roleAll = () => {
+  connection.query('SELECT * FROM role', function(err, res) {
+    if (err) throw err;
+    console.log(res);
+    connection.end();
+  });
+};
+
+employeeAll = () => {
+  connection.query('SELECT a.id, a.first_name, a.last_name, CONCAT(b.first_name, " ", b.last_name) AS manager FROM employee a LEFT JOIN employee b ON a.manager_id = b.id', function(err, res) {
+    if (err) throw err;
+    console.log(res);
+    connection.end();
+  });
+};
